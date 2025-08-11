8166f0270bc9cace9872841596b5e537cedc8308
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-08-11 14:12:04 -0700

    update file structure to be more flexible

--- scraper.js
+++ scraper.js
@@ -1,52 +1,63 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const dest = path.join(process.cwd(), "tiles");
-console.log("Writing to", dest);
-
 const threadCount = 32;
-const baseUrl = "https://game-cdn.appsample.com/gim/map-teyvat/v58-rc2";
-const ranges = {
-	10: {
-		x: [-2, 1],
-		y: [-2, 1],
-	},
-	11: {
-		x: [-4, 3],
-		y: [-4, 2],
-	},
-	12: {
-		x: [-8, 7],
-		y: [-8, 5],
-	},
-	13: {
-		x: [-16, 15],
-		y: [-16, 10],
-	},
-	14: {
-		x: [-32, 31],
-		y: [-32, 20],
-	},
-	15: {
-		x: [-64, 63],
-		y: [-64, 41],
+
+const maps = {
+	teyvat: {
+		baseUrl: "https://game-cdn.appsample.com/gim/map-teyvat/v58-rc2",
+		outputFolder: path.join(process.cwd(), "teyvat"),
+		ranges: {
+			10: {
+				x: [-2, 1],
+				y: [-2, 1],
+			},
+			11: {
+				x: [-4, 3],
+				y: [-4, 2],
+			},
+			12: {
+				x: [-8, 7],
+				y: [-8, 5],
+			},
+			13: {
+				x: [-16, 15],
+				y: [-16, 10],
+			},
+			14: {
+				x: [-32, 31],
+				y: [-32, 20],
+			},
+			15: {
+				x: [-64, 63],
+				y: [-64, 41],
+			},
+		},
 	},
 };
 
+console.log("Output folders...");
+for (const [mapName, { outputFolder }] of Object.entries(maps)) {
+	if (!fs.existsSync(outputFolder)) {
+		fs.mkdirSync(outputFolder, { recursive: true });
+	}
+	console.log(`${mapName}: ${outputFolder}`);
+}
+
 class DownloadThread {
 	constructor(i) {
-		this.urls = [];
+		this.downloads = [];
 		this.threadNum = i;
 	}
 	add(url) {
-		this.urls.push(url);
+		this.downloads.push(url);
 	}
 	async start() {
-		for (const url of this.urls) {
-			console.log(`[${this.threadNum}] Downloading`, url.dest);
-			const res = await fetch(`${baseUrl}/${url.src}`);
+		for (const download of this.downloads) {
+			console.log(`[${this.threadNum}] Downloading`, download.dest);
+			const res = await fetch(download.src);
 			const buffer = Buffer.from(await res.arrayBuffer());
-			fs.writeFileSync(`${dest}/${url.dest}`, buffer);
+			fs.writeFileSync(download.dest, buffer);
 		}
 	}
 }
@@ -56,19 +67,26 @@ const threads = [];
 for (let i = 0; i < threadCount; i++) {
 	threads.push(new DownloadThread(i));
 }
-for (const [scale, range] of Object.entries(ranges)) {
-	if (!fs.existsSync(`${dest}/${scale}`)) {
-		fs.mkdirSync(`${dest}/${scale}`, { recursive: true });
-	}
-	for (let x = range.x[0]; x <= range.x[1]; x++) {
-		for (let y = range.y[0]; y <= range.y[1]; y++) {
-			threads[t++ % threadCount].add({
-				src: `${scale}/tile-${x}_${y}.jpg`,
-				dest: `${scale}/${x}_${y}.jpg`,
-			});
+const addDownload = (src, dest) => {
+	threads[t++ % threadCount].add({ src, dest });
+};
+
+for (const { baseUrl, outputFolder, ranges } of Object.values(maps)) {
+	for (const [scale, range] of Object.entries(ranges)) {
+		if (!fs.existsSync(`${outputFolder}/${scale}`)) {
+			fs.mkdirSync(`${outputFolder}/${scale}`, { recursive: true });
+		}
+		for (let x = range.x[0]; x <= range.x[1]; x++) {
+			for (let y = range.y[0]; y <= range.y[1]; y++) {
+				addDownload(
+					`${baseUrl}/${scale}/tile-${x}_${y}.jpg`,
+					`${outputFolder}/${scale}/${x}_${y}.jpg`,
+				);
+			}
 		}
 	}
 }
+
 const startTime = Date.now();
 const promises = threads.map((t) => {
 	return new Promise((r) => {
